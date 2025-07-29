1a14e5b86889287093ad54468b2da958184a7121
jhk01007 <jhk001007@naver.com>
2025-07-30 00:32:51 +0900

    메시지 저장 API 개발

--- src/main/java/com/example/chatserver/chat/controller/StompController.java
+++ src/main/java/com/example/chatserver/chat/controller/StompController.java
@@ -1,6 +1,7 @@
 package com.example.chatserver.chat.controller;
 
 import com.example.chatserver.chat.dto.ChatMessageReqDto;
+import com.example.chatserver.chat.service.ChatService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
@@ -18,6 +19,8 @@ public class StompController {
 
     private final SimpMessageSendingOperations messageTemplate;
 
+    private final ChatService chatService;
+
     // 방법1. MessageMapping(수신)과 SendTo(topic에 메시지 전달)를 한꺼번에 처리
 //    @MessageMapping("/{roomId}") // 클라이언트에서 특정 publish/roomId 형태로 메시지를 발행시 MessageMapping 수신
 //    @SendTo("/topic/{roomId}") // 해당 roomId에 메시지를 발행하여 구독중인 클라이언트에게 메시지 전송
@@ -31,6 +34,7 @@ public class StompController {
     @MessageMapping("/{roomId}")
     public void sendMessage(@DestinationVariable Long roomId, ChatMessageReqDto chatMessageReqDto) {
         log.info(chatMessageReqDto.getMessage());
+        chatService.saveMessage(roomId, chatMessageReqDto);
         messageTemplate.convertAndSend("/topic/" + roomId, chatMessageReqDto); // @SendTo 어노테이션 대체
     }
 }
--- src/main/java/com/example/chatserver/chat/service/ChatService.java
+++ src/main/java/com/example/chatserver/chat/service/ChatService.java
@@ -1,13 +1,22 @@
 package com.example.chatserver.chat.service;
 
+import com.example.chatserver.chat.domain.ChatMessage;
+import com.example.chatserver.chat.domain.ChatParticipant;
+import com.example.chatserver.chat.domain.ChatRoom;
+import com.example.chatserver.chat.domain.ReadStatus;
+import com.example.chatserver.chat.dto.ChatMessageReqDto;
 import com.example.chatserver.chat.repository.ChatMessageRepository;
 import com.example.chatserver.chat.repository.ChatParticipantRepository;
 import com.example.chatserver.chat.repository.ChatRoomRepository;
 import com.example.chatserver.chat.repository.ReadStatusRepository;
+import com.example.chatserver.member.domain.Member;
 import com.example.chatserver.member.repository.MemberRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
@@ -18,7 +27,40 @@ public class ChatService {
     private final ChatRoomRepository chatRoomRepository;
     private final ChatParticipantRepository chatParticipantRepository;
     private final ChatMessageRepository chatMessageRepository;
-    private final ReadStatusRepository repository;
+    private final ReadStatusRepository readStatusRepository;
     // Member
     private final MemberRepository memberRepository;
+
+    public void saveMessage(Long id, ChatMessageReqDto chatMessageReqDto) {
+        // 1. 채팅방 조회
+        ChatRoom chatRoom = chatRoomRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("room cannot be found"));
+
+        // 2. 보낸사람 조회
+        Member sender = memberRepository.findByEmail(chatMessageReqDto.getSenderEmail())
+                .orElseThrow(() -> new EntityNotFoundException("member cannot be found"));
+
+        // 3. 메시지 저장
+        ChatMessage chatMessage = ChatMessage.builder()
+                .chatRoom(chatRoom)
+                .member(sender)
+                .content(chatMessageReqDto.getMessage())
+                .build();
+
+        chatMessageRepository.save(chatMessage);
+
+        // 4. 사용자별로 읽음여부 저장
+        List<ChatParticipant> chatParticipants = chatParticipantRepository.findByChatRoom(chatRoom);
+
+        for (ChatParticipant c : chatParticipants) {
+
+            ReadStatus readStatus = ReadStatus.builder()
+                    .chatRoom(chatRoom)
+                    .member(c.getMember())
+                    .chatMessage(chatMessage)
+                    .isRead(c.getMember().equals(sender)) // 보낸사람은 true 아니면 false. JPA에선 객체의 동일성을 보장해줌
+                    .build();
+            readStatusRepository.save(readStatus);
+        }
+    }
 }
